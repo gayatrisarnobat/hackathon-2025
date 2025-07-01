e9fcb53a7694fd417cee783de96c3edf6c84325c
Gayatri Sarnobat <gayatrisarnobat@Gayatris-MacBook-Air.local>
2025-07-01 23:20:53 +0530

    Initial Commit

---
+++ TradingApp/TradingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingAppApp.swift
+//  TradingApp
+//
+//  Created by Gayatri Sarnobat on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TradingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TradingAppTests/TradingAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TradingAppTests.swift
+//  TradingAppTests
+//
+//  Created by Gayatri Sarnobat on 01/07/25.
+//
+
+import Testing
+
+struct TradingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
